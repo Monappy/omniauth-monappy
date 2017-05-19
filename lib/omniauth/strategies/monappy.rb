b7ace23f94e77fc064b897f2f1ab463c8cc0dc27
Palon <palon7@gmail.com>
2017-05-19 21:17:01 +0900

    Fix callback

--- lib/omniauth/strategies/monappy.rb
+++ lib/omniauth/strategies/monappy.rb
@@ -1,26 +1,30 @@
 require "omniauth-oauth2"
 
 module OmniAuth
-  module Strategies
-    class Monappy < OmniAuth::Strategies::OAuth2
-        option :name, :monappy
+    module Strategies
+        class Monappy < OmniAuth::Strategies::OAuth2
+            option :name, :monappy
 
-        option :client_options, site: 'https://api.monappy.jp', authorize_path: '/oauth/authorize'
+            option :client_options, site: 'https://api.monappy.jp', authorize_path: '/oauth/authorize'
 
-        uid { raw_info['User']['id'] }
+            uid { raw_info['User']['id'] }
 
-        info do
-            {
-                id: raw_info['User']['id'],
-                id: raw_info['User']['nickname'],
-                id: raw_info['User']['mail'],
-                id: raw_info['User']['id'],
-            }
-        end
+            info do
+                {
+                    id: raw_info['User']['id'],
+                    id: raw_info['User']['nickname'],
+                    id: raw_info['User']['mail'],
+                    id: raw_info['User']['id'],
+                }
+            end
 
-        def raw_info
-            @raw_info ||= access_token.get('/oauth/userinfo').parsed
+            def raw_info
+                @raw_info ||= access_token.get('/oauth/userinfo').parsed
+            end
+
+            def callback_url
+                full_host + script_name + callback_path
+            end
         end
     end
-  end
 end
